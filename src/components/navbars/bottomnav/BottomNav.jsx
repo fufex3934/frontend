a4f4f761de4dae6940bf5fd78cc53d9dee0acf8c
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-05-12 22:58:22 +0300

    Clean Code Commit

---
+++ src/components/navbars/bottomnav/BottomNav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BottomNav = () => {
+  return (
+    <></>
+  )
+}
+
+export default BottomNav
--- src/components/navbars/topnav/TopNav.jsx
+++ src/components/navbars/topnav/TopNav.jsx
@@ -6,16 +6,16 @@ const TopNav = () => {
   return (
     <div className='bg-gray-800 w-full flex p-2 max-[942px]:hidden'>
       <div className='float-left flex-auto ml-[100px] flex'>
-        <a href='tel:+25164963831' className='text-white px-3 flex'> <HiOutlinePhoneArrowDownLeft className='text-green-500/100 mr-2  text-2xl' />Call on: 1800 123 4567</a>
+        <a href='tel:+25164963831' className='text-white transition ease-in-out delay-250 decoration-0 px-3 flex hover:drop-shadow-2xl hover:bg-white hover:rounded-full  hover:shadow-white-500/50  hover:text-blue-400 hover hover:translate-x-1 hover:-translate-y-1'> <HiOutlinePhoneArrowDownLeft className='text-green-500/100 mr-2  text-2xl' />Call on: 1800 123 4567</a>
         <span className='text-white px-3 flex'><BiAlarm className="text-green-500/100 mr-1 text-2xl" />Open Hours: Mon - Fri 8.00 am - 6.00 pm</span>
       </div>
       <div className='float-right flex-auto ml-[300px]'>
-          <a href="#" className='text-white px-5 hover:text-red-600/100'>Council</a>
-          <a href="#" className='text-white px-5 hover:text-red-600/100'>Vacancies</a>
-          <a href="#" className='text-white px-5 hover:text-red-600/100'>Complaints</a>
+        <a href="#" className='text-white decoration-0 px-5 hover:text-blue-400'>Council</a>
+        <a href="#" className='text-white decoration-0 px-5 hover:text-blue-400'>Vacancies</a>
+        <a href="#" className='text-white decoration-0 px-5 hover:text-blue-400'>Complaints</a>
       </div>
     </div>
   )
 }
 
-export default TopNav
+export default TopNav;
