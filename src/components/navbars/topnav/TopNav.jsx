a0a52156e46a2520c10a6107f94cdc0dfc8265f0
Fufa Wakgari <fufawakgari174@gmail.com>
2023-05-13 20:35:55 +0300

    some

--- src/components/navbars/topnav/TopNav.jsx
+++ src/components/navbars/topnav/TopNav.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-import { HiOutlinePhoneArrowDownLeft } from "react-icons/hi2"
-import { BiAlarm } from "react-icons/bi"
+import React from 'react';
+import { HiOutlinePhoneArrowDownLeft } from 'react-icons/hi';
+import { BiAlarm } from 'react-icons/bi';
 
 const TopNav = () => {
-    return (
-        <div className='bg-gray-800 w-full flex p-2 max-[942px]:hidden'>
-            <div className='float-left flex-auto ml-[100px] flex'>
-                <a href='tel:+25164963831' className='text-white transition ease-in-out delay-250 decoration-0 px-3 flex hover:drop-shadow-2xl hover:bg-white hover:rounded-full  hover:shadow-white-500/50  hover:text-blue-400 hover hover:translate-x-1 hover:-translate-y-1'> <HiOutlinePhoneArrowDownLeft className='text-green-500/100 mr-2  text-2xl' />Call on: 1800 123 4567</a>
-                <span className='text-white px-3 flex'><BiAlarm className="text-green-500/100 mr-1 text-2xl" />Open Hours: Mon - Fri 8.00 am - 6.00 pm</span>
-            </div>
-            <div className='float-right flex-auto ml-[300px]'>
-                <a href="#" className='text-white decoration-0 px-5 hover:text-blue-400'>Council</a>
-                <a href="#" className='text-white decoration-0 px-5 hover:text-blue-400'>Vacancies</a>
-                <a href="#" className='text-white decoration-0 px-5 hover:text-blue-400'>Complaints</a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="bg-gray-800 w-full p-2   ">
+      <div className="flex-auto ml-6 flex items-center">
+        <a
+          href="tel:+25164963831"
+          className="text-white transition ease-in-out duration-300 decoration-0 flex items-center hover:text-blue-400"
+        >
+          <HiOutlinePhoneArrowDownLeft className="text-green-500 mr-2 text-2xl" />
+          <span className="whitespace-nowrap">Call on: 1800 123 4567</span>
+        </a>
+        <span className="text-white ml-6 flex items-center">
+          <BiAlarm className="text-green-500 mr-2 text-2xl" />
+          <span className="whitespace-nowrap">Open Hours: Mon - Fri 8.00 am - 6.00 pm</span>
+        </span>
+      </div>
+      <div className="flex-auto ml-auto mr-12">
+        <a href="#" className="text-white decoration-0 ml-6 hover:text-blue-400">
+          Council
+        </a>
+        <a href="#" className="text-white decoration-0 ml-6 hover:text-blue-400">
+          Vacancies
+        </a>
+        <a href="#" className="text-white decoration-0 ml-6 hover:text-blue-400">
+          Complaints
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default TopNav;
