f7079aa9b982f739c9898e76abb82585e007ded8
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-05-13 19:17:26 +0300

    Toooya,

--- src/components/navbars/topnav/TopNav.jsx
+++ src/components/navbars/topnav/TopNav.jsx
@@ -6,8 +6,8 @@ const TopNav = () => {
     return (
         <div className='bg-gray-800 w-full flex p-2 max-[942px]:hidden'>
             <div className='float-left flex-auto ml-[100px] flex'>
-                <a href='tel:+25164963831' className='text-white transition ease-in-out delay-250 decoration-0 px-3 flex hover:drop-shadow-2xl hover:bg-white hover:rounded-full  hover:shadow-white-500/50  hover:text-blue-400 hover hover:translate-x-1 hover:-translate-y-1'> <HiOutlinePhoneArrowDownLeft className='text-green-500/100 mr-2  text-2xl' />Call on: 1800 123 4567</a>
-                <span className='text-white px-3 flex'><BiAlarm className="text-green-500/100 mr-1 text-2xl" />Open Hours: Mon - Fri 8.00 am - 6.00 pm</span>
+                <a href='tel:+25164963831' className='text-white whitespace-nowrap transition ease-in-out delay-250 decoration-0 px-3 flex hover:drop-shadow-2xl hover:bg-white hover:rounded-full  hover:shadow-white-500/50  hover:text-blue-400 hover hover:translate-x-1 hover:-translate-y-1'> <HiOutlinePhoneArrowDownLeft className='text-green-500/100 mr-2  text-2xl' />Call on: 1800 123 4567</a>
+                <span className='text-white whitespace-nowrap px-3 flex'><BiAlarm className="text-green-500/100 mr-1 text-2xl" />Open Hours: Mon - Fri 8.00 am - 6.00 pm</span>
             </div>
             <div className='float-right flex-auto ml-[300px]'>
                 <a href="#" className='text-white decoration-0 px-5 hover:text-blue-400'>Council</a>
