e476928689d4bc9fb161bba29c54489eeb6abccc
Fufa Wakgari <fufawakgari174@gmail.com>
2023-05-14 14:28:33 +0300

    I cannot make navbar responsive

--- src/components/navbars/bottomnav/BottomNav.jsx
+++ src/components/navbars/bottomnav/BottomNav.jsx
@@ -5,7 +5,7 @@ import '../../styles/navbar.css';
 
 const BottomNavbar = () => {
   const [showNavbar, setShowNavbar] = useState(true);
-
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
@@ -37,8 +37,33 @@ const BottomNavbar = () => {
 <h1 className='text-gray-500 font-bold flex-grow mr-3'>HGCAOffice</h1>
 </a>
     
+<div className="block lg:hidden">
+  <button
+    onClick={() => setIsOpen(!isOpen)}
+    className={`flex items-center px-3 py-2 ml-40 rounded text-black-500 hover:text-black-400 ${isOpen ? 'rotate-180' : ''}`}
+  >
+    <svg
+      className={`fill-current h-4 w-4 ${isOpen ? 'hidden' : 'block'}`}
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+    </svg>
+    <svg
+      className={`fill-current h-4 w-4 ${isOpen ? 'block' : 'hidden'}`}
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+    </svg>
+  </button>
+</div>
+
   
-  <li className="group z-10 relative dropdown  text-gray-600 hover:text-orange-600 cursor-pointer text-md font-serif ">
+     <div
+       className={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${isOpen ? "block" : "hidden"}`}
+     >
+      <li className="group z-10  relative dropdown  text-gray-600 hover:text-orange-600 cursor-pointer text-md font-serif ">
   <a  className="inline-flex  w-full justify-center gap-x-0  bg-white px-1 py-2 text-sm " id="menu-button" aria-expanded="true" aria-haspopup="true">
       Home
       <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -177,7 +202,8 @@ const BottomNavbar = () => {
     <button className="bg-transparent hover:bg-orange-700 text-blue-500 font-semibold hover:text-white py-1 px-1 border border-blue-700 rounded overflow-hidden whitespace-nowrap transition-colors duration-300 ease-in-out">
       Report an Issue
     </button>
-  </li>   
+  </li>  
+      </div> 
 </ul>
 
     </div>
