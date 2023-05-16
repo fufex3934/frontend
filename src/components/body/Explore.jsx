d33681bcd7e848a6fc914e5d093959c650f22b4d
Fufa Wakgari <fufawakgari174@gmail.com>
2023-05-16 17:00:40 +0300

    cat code

--- src/components/body/Explore.jsx
+++ src/components/body/Explore.jsx
@@ -18,14 +18,14 @@ const Explore = () => {
                         </h3>
                     </div>
                     <div className="text-center float-right mt-4" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
-                        <button class="bg-black text-3xl font-serif hover:bg-orange-700 text-white  py-2 px-4 rounded-0">
+                        <button className="bg-black text-3xl font-serif hover:bg-orange-700 text-white  py-2 px-4 rounded-0">
                             Explore Service
                         </button>
                     </div>
                 </div>
             </div>
-            <div class="grid ms-[150px] me-[150px] mt-[90px]">
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-5 mb-8">
+            <div className="grid ms-[150px] me-[150px] mt-[90px]">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mb-8">
                     <div className='text-center image relative' data-aos-duration="1000" data-aos="fade-up" data-aos-easing="linear">
                         <img className='' src="images/explore.jpg" alt="" />
                         <a href="" className='text-lg md:text-xl absolute -bottom-10 text-white bg-black p-3 hover:shadow-lg left-20'>Service Departments</a>
@@ -45,3 +45,4 @@ const Explore = () => {
     )
 }
 
+export default Explore;
