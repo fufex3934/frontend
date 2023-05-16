5f48ff71202d3e269282cea734dbb4df745b3bf0
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-05-16 11:40:26 +0300

    Animation Commited

--- src/components/body/Explore.jsx
+++ src/components/body/Explore.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Explore.css';
+import 'aos/dist/aos.css'
 
 const Explore = () => {
+   
     return (
         <>
             <div className='block m-[150px] ms-[110px] p-5'>
 
-                <div className='inline w-full '>
+                <div className='inline w-full'>
                     <div className="block float-left">
-                        <h3 className="text-4xl font-bold text-slate-600 font-sans">
+                        <h3 className="h3 text-4xl font-bold text-slate-600 font-sans">
                             Let’s explore local services,
                         </h3>
                         <h3 className="text-4xl font-bold text-slate-600 font-sans">
@@ -24,15 +26,16 @@ const Explore = () => {
             </div>
             <div class="grid mx-[150px] mb-[100px]">
                 <div class="h-[200px] grid grid-rows-1 grid-flow-col gap-5 mb-8">
-                    <div className='text-center image relative'>
+                    <div className='text-center image relative' data-aos-duration="1000" data-aos="fade-right" data-aos-easing="linear">
                         <img className='' src="images/explore.jpg" alt="" />
                         <a href="" className='text-xl absolute -bottom-20 text-white bg-black p-3 hover:shadow-lg left-20'>Service Departments</a>
                     </div>
-                    <div className='text-center image relative'>
+                    <div className='text-center image relative' data-aos="zoom-in"
+                        data-aos-duration="1500">
                         <img src="images/explore.jpg" alt="" />
                         <a href="" className='text-xl absolute -bottom-20 text-white bg-black p-3 hover:shadow-lg left-20'>Service Departments</a>
                     </div>
-                    <div className='text-center image relative'>
+                    <div className='text-center image relative' data-aos-duration="1000" data-aos="fade-left" data-aos-easing="linear">
                         <img src="images/explore.jpg" alt="" />
                         <a href="" className='text-xl absolute -bottom-20 text-white bg-black p-3 hover:shadow-lg left-20'>Service Departments</a>
                     </div>
@@ -41,3 +44,4 @@ const Explore = () => {
         </>
     )
 }
+export default Explore;
--- src/components/body/MeetIdeologyLeader.jsx
+++ src/components/body/MeetIdeologyLeader.jsx
@@ -10,19 +10,19 @@ const MeetIdeologyLeader = () => {
     <>
     <div className='main'>
         <div className='main-text'>
-            <h1>
+            <h1 className='h1'data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500">
                 Meet Ideological leader for youth generation
             </h1>
-            <p className='p1'>
+            <p className='p1' data-aos-duration="1000" data-aos="fade-up" data-aos-easing="linear">
                 Mayor Ordin Bedri is committed to solving problems for town
                 people across the state under his leadership.
             </p>
-            <p className='p2'>
+            <p className='p2' data-aos-duration="1000" data-aos="fade-up" data-aos-easing="linear">
                 Expanding access to affordable healthcare, improving skills, respecting 
                 working families as the City’s 20th mayor. 
                 Mayor Ordin was won in the serve a sixth term on Octorber 7, 2018.
             </p>
-            <p className='qouet'>
+            <p className='qouet' data-aos-duration="1000" data-aos="fade-up" data-aos-easing="linear">
                 “Stand at the top of a cliff and jump off and build your wings on the way down.”<br/>
                 <span>– Ordin Bedri, Mayor</span>
             </p>
@@ -31,8 +31,9 @@ const MeetIdeologyLeader = () => {
                     <div className='triangle'></div>
                 </div>
             </a>
-            <span className='span'>Video Intro<br/>
-                About Our Municipal</span>
+            <span className='span' data-aos-duration="1000" data-aos="fade-up" data-aos-easing="linear">Video Intro<br/>
+                About Our Municipal
+            </span>
         </div>
         <div className='main-image'>
             <img src="images/mayor.jpeg" alt="" className="person" data-aos-duration="1000" data-aos="fade-up" data-aos-easing="linear" />
