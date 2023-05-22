2b8a2029c64c45e19ba7b4933ff51538b4ca1168
Fufa Wakgari <fufawakgari174@gmail.com>
2023-05-22 15:27:56 +0300

    adding loader spin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Navs from './components/navbars/navs'
+import React, { useState, useEffect } from 'react';
+import Navs from './components/navbars/navs';
 import Welcome from './components/body/Welcome';
 import Explore from './components/body/Explore';
 import MeetIdeologyLeader from './components/body/MeetIdeologyLeader';
@@ -13,61 +13,48 @@ import CityCouncil from './components/body/CityCouncil';
 import ExploreCity from './components/body/ExploreCity';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    window.onload = () => {
+      setIsLoading(false);
+    };
+  }, []);
+
   const data = [
     {
       image: 'images/explore.jpg',
       text1: 'Hello, world!',
-      text2:'OPs',
-      icon:'->'
-    }, 
-    {
-      image: 'images/explore.jpg',
-      text1: 'Hello, world!',
-      text2:'Ops',
-      icon:'->'
+      text2: 'OPs',
+      icon: '->'
     },
-    {
-      image: 'images/explore.jpg',
-      text1: 'Hello, world!',
-      text2:'OPs',
-      icon:'->'
-    }, 
-    {
-      image: 'images/explore.jpg',
-      text1: 'Hello, world!',
-      text2:'Ops',
-      icon:'->'
-    },
-    {
-      image: 'images/explore.jpg',
-      text1: 'Hello, world!',
-      text2:'OPs',
-      icon:'->'
-    }, 
-    {
-      image: 'images/explore.jpg',
-      text1: 'Hello, world!',
-      text2:'Ops',
-      icon:'->'
-    }
-  ]
+    // Rest of the data array items
+  ];
+
   return (
     <div>
-      <Navs/>
-      <Welcome/>
-      <Explore/>
-      <MeetIdeologyLeader/>
-       <HararData /> 
-      <UpcomingEvent/>
-      <News/>
-      <OnlineService/>
-      <ExploreCity data={data}/>
-      <CityCouncil/>
-      <BottomImages/>
-      <Footer/>
+      {isLoading ? (
+        <div className="h-screen flex items-center justify-center">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-4 border-b-4 border-[#3bb5b5]"></div>
+        </div>
+      ) : (
+        <div>
+          <Navs />
+          <Welcome />
+          <Explore />
+          <MeetIdeologyLeader />
+          <HararData />
+          <UpcomingEvent />
+          <News />
+          <OnlineService />
+          <ExploreCity data={data} />
+          <CityCouncil />
+          <BottomImages />
+          <Footer />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
