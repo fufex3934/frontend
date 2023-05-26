1982f8877422abbe5ac331d02c916df68dc02a22
Fufa Wakgari <fufawakgari174@gmail.com>
2023-05-26 22:48:07 +0300

    finishing media query of city council

--- src/components/body/BottomImages.jsx
+++ src/components/body/BottomImages.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function BottomImages() {
   return (
-    <div className='flex justify-between  mt-24  px-20 mb-10 pb-14'>
+    <div className='flex justify-between  px-20 mb-10 pb-14 mt-[180vh] md:mt-[25vh] lg:mt-24'>
       <img className='mr-24 w-16 h-16' src="/images/about_logo_1.png" alt="about1" />
       <img  className='mr-24  w-16 h-16' src="/images/about_logo_2.png" alt="about2" />
       <img  className='mr-24  w-16 h-16' src="/images/about_logo_3.png" alt="about3" />
