99a73ca14be09be0c58478131f94b6b95d14c076
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-05-27 21:48:40 +0300

    End Of Media Query

--- src/App.jsx
+++ src/App.jsx
@@ -24,46 +24,46 @@ const App = () => {
 
   const data = [
     {
-      image: 'images/c12.jpg',
-      text1: 'Hello, world!',
-      text2: 'OPs',
-      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-red-500'/>
+      image: 'images/hyenas.jpg',
+      text1: 'Hyenas',
+      text2: 'Animal Exhibition',
+      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-[#3bb5b5]'/>
     },
     {
-      image: 'images/car1.jpg',
-      text1: 'Hello, world!',
-      text2: 'OPs',
-      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-red-500' />
+      image: 'images/hararicity.jpg',
+      text1: 'Jugol Building!',
+      text2: 'Historical Building',
+      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-[#3bb5b5]' />
     },
     {
-      image: 'images/car10.jpg',
-      text1: 'Hello, world!',
-      text2: 'OPs',
-      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-red-500' />
+      image: 'images/musume.jpg',
+      text1: 'Bussiness, Government!',
+      text2: 'Harari Art Musume',
+      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-[#3bb5b5]' />
     },
     {
       image: 'images/car6.jpg',
-      text1: 'Hello, world!',
-      text2: 'OPs',
-      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-red-500' />
+      text1: 'Business',
+      text2: 'Cinema Teater',
+      icon: <BsArrowRightCircle className='text-4xl  fill-white hover:fill-[#3bb5b5]' />
     },
     {
       image: 'images/car7.jpg',
-      text1: 'Hello, world!',
-      text2: 'OPs',
-      icon: <BsArrowRightCircle className='text-4xl fill-white hover:fill-red-500' />
-    },
-    {
-      image: 'images/car8.jpg',
-      text1: 'Hello, world!',
-      text2: 'OPs',
-      icon: <BsArrowRightCircle className='text-4xl fill-white hover:fill-red-500' />
+      text1: 'Culture,Event',
+      text2: 'City Of Harari',
+      icon: <BsArrowRightCircle className='text-4xl fill-white hover:fill-[#3bb5b5]' />
     },
     {
       image: 'images/car13.jpg',
-      text1: 'Hello, world!',
-      text2: 'OPs',
-      icon: <BsArrowRightCircle className='text-4xl fill-white hover:fill-red-500' />
+      text1: 'Culture ,Public Place',
+      text2: 'Business Agence',
+      icon: <BsArrowRightCircle className='text-4xl fill-white hover:fill-[#3bb5b5]' />
+    },
+    {
+      image: 'images/car8.jpg',
+      text1: 'Holydays ,Festivals',
+      text2: 'Shewal Eid Holyday/Festival',
+      icon: <BsArrowRightCircle className='text-4xl fill-white hover:fill-[#3bb5b5]' />
     },
     // Rest of the data array items
   ];
