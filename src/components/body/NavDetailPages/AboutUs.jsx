cb332403919109e1fb3c10fa4123fcb27cd88a88
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-05-30 14:55:47 +0300

    Minor Commit

--- src/components/body/NavDetailPages/AboutUs.jsx
+++ src/components/body/NavDetailPages/AboutUs.jsx
@@ -3,6 +3,17 @@ import Footer from '../Footer'
 import '../../body/css/AboutUs.css'
 import Navs from '../../navbars/navs'
 
+const data=[
+  {
+    heading:"Mission",
+    content:"To play a leading role in the Government Communication, Information and Media Sector and to effectively coordinate the success of our vision by developing public awareness on basic policies, strategies, plans and programs through a comprehensive exchange of information among the people."
+  },
+  {
+    heading:"Vision",
+    content:"To see the people of 2023 come to an agreement on basic regional and national issues with a rich, multifaceted participation."
+  }
+]
+
 const AboutUs = () => {
   return (
     <>
@@ -11,38 +22,43 @@ const AboutUs = () => {
         <div className="heading-top">
           <h1>Harari Government Communication Affairs Office</h1>
         </div>
-        <div className='mission'>
-          <div className='heading'>
-            <h1>Mission</h1>
+        <div className="both">
+          <div className="left-content">
+            {
+              data.map((item) => (
+                <div key={item.heading} className="content-container">
+                  <div className='heading'>
+                    <h1>{item.heading}</h1>
+                  </div>
+                  <div className='content'>
+                    <p>{item.content}</p>
+                  </div>
+                </div>
+              ))
+            }
+            <div className="value">
+              <div className="heading">
+                <h1>Core Values</h1>
+              </div>
+              <div className="content">
+                <ul>
+                  <li>Sincerity</li>
+                  <li>Loyalty</li>
+                  <li>Responsibility</li>
+                  <li>Respect and Unity</li>
+                  <li>Faith in the Power of Victory</li>
+                  <li>Collaboration and teamwork</li>
+                  <li>Leadership qualifications</li>
+                  <li>Taking Responsibility</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className=' content'>
-            <p>To play a leading role in the Government Communication, Information and Media Sector and to effectively coordinate the success of our vision by developing public awareness on basic policies, strategies, plans and programs through a comprehensive exchange of information among the people.</p>
-          </div>
-        </div>
-        <div className='vission'>
-          <div className='heading'>
-            <h1>Vission</h1>
-          </div>
-          <div className='content'>
-            <p>
-              "To see the people of 2023 come to an agreement on basic regional and national issues with a rich, multifaceted participation."
-            </p>
-          </div>
-        </div>
-        <div className="goal">
-          <div className='heading'>
-            <h1>Goal</h1>
-          </div>
-          <div className="content">
-            <p>Goals</p>
-          </div>
-        </div>
-        <div className="value">
-          <div className="heading">
-            <h1>Values</h1>
-          </div>
-          <div className="content">
-            <p>Values</p>
+          <div className="right-map">
+            <div className="header">
+              <h1>Our Location</h1>
+            </div>
+            <div className="map"></div>
           </div>
         </div>
       </div>
