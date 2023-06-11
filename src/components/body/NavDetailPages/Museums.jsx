3ccf4cb599d893c916b1910bbd6e66e25926540f
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-06-11 22:14:55 +0300

    Before Ops

--- src/components/body/NavDetailPages/Museums.jsx
+++ src/components/body/NavDetailPages/Museums.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Navs from '../../navbars/navs'
 import Footer from '../Footer'
-
+import '../css/Museums.css'
+import {Link} from 'react-router-dom'
 const Museums = () => {
   return (
     <>
     <Navs/>
-    <div>
-    Museums Page
+    <div className='musuem'>
+        <h1 className='text-2xl font-mono'>Experience 5,000 Years Of</h1>
+        <h1 className='text-2xl mb-6 font-mono'>Art At Sherif Private</h1>
+        <Link to='https://harari.gov.et/hr/landmarks/sherif-private-musuem' target='_blank' className='link text-xl font-serif px-4 py-2'>Explore</Link>
     </div>
+    <div className="latest-exhibition"></div>
     <Footer/>
     </>
   )
