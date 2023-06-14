9e4ab5b7e3ee2aa1ea748ea2b90d5f12389d448d
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-06-14 12:32:58 +0300

    Service Department Commit

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ import OfficesDetails from './components/body/bodyDetailsPage/OfficesDetails';
 import Gallery from './components/body/Gallery';
 import Geography from './components/body/Geography'
 import Services from './components/body/Services';
+import ServiceDepartment from './components/body/ServiceDepartment';
 
 const App = () => {
   useEffect(() => {
@@ -48,7 +49,7 @@ const App = () => {
       <Route path='/about-leadership' element={<Leadership />} />
       <Route path='/about-history' element={<History />} />
       <Route path='/timely-news' element={<News />} />
-      <Route path='/contact-us' element={<ContactUs/>}/>
+      <Route path='/contact-us' element={<ContactUs />} />
       <Route path='/timely-emergence' element={<Emergency />} />
       <Route path='/economy-policy' element={<Policies />} />
       <Route path='/economy-sectors' element={<Sectors />} />
@@ -59,24 +60,25 @@ const App = () => {
       <Route path='/tourism-mon' element={<Monuments />} />
       <Route path='/tourism-mus' element={<Museums />} />
       <Route path='/tourism-animal' element={<AnimalExhibition />} />
-      <Route path= '/upcoming-detail1' element={<UpcomingEvent1/>}/>
-      <Route path= '/upcoming-detail2' element={<UpcomingEvent2/>}/>
-      <Route path='/news/:slug' element = {<HomeNewsDetails/>}/>
-      <Route path='/harari-news' element = {<HararifNews/>}/>
-      <Route path='/harari-news/:slug' element = {<HarariNewsDetails/>}/>
-      <Route path='/national-news' element = {<NationalNews/>}/>
-      <Route path='/national-news/:slug' element = {<NationalNewsDetails/>}/>
-      <Route path='/africa-news' element = {<AfricaNews/>}/>
-      <Route path='/africa-news/:slug' element = {<AfricaNewsDetails/>}/>
-      <Route path='/international-news' element = {<InternationalNews/>}/>
-      <Route path='/international-news/:slug' element = {<InternationalNewsDetails/>}/>
-      <Route path='/woreda' element = {<Woreda/>}/>
-      <Route path='/woreda-details/:id' element = {<WoredaDetails/>}/>
-      <Route path='/offices' element = {<Offices/>}/>
-      <Route path='/office-details/:id' element = {<OfficesDetails/>}/>
-      <Route path='/gallery' element = {<Gallery/>}/>
-      <Route path='/geography' element={<Geography/>}/>
-      <Route path='/services' element = {<Services/>}/>
+      <Route path='/upcoming-detail1' element={<UpcomingEvent1 />} />
+      <Route path='/upcoming-detail2' element={<UpcomingEvent2 />} />
+      <Route path='/news/:slug' element={<HomeNewsDetails />} />
+      <Route path='/harari-news' element={<HararifNews />} />
+      <Route path='/harari-news/:slug' element={<HarariNewsDetails />} />
+      <Route path='/national-news' element={<NationalNews />} />
+      <Route path='/national-news/:slug' element={<NationalNewsDetails />} />
+      <Route path='/africa-news' element={<AfricaNews />} />
+      <Route path='/africa-news/:slug' element={<AfricaNewsDetails />} />
+      <Route path='/international-news' element={<InternationalNews />} />
+      <Route path='/international-news/:slug' element={<InternationalNewsDetails />} />
+      <Route path='/woreda' element={<Woreda />} />
+      <Route path='/woreda-details/:id' element={<WoredaDetails />} />
+      <Route path='/offices' element={<Offices />} />
+      <Route path='/office-details/:id' element={<OfficesDetails />} />
+      <Route path='/gallery' element={<Gallery />} />
+      <Route path='/geography' element={<Geography />} />
+      <Route path='/services' element={<Services />} />
+      <Route path='/service-department' element={<ServiceDepartment />} />
       <Route path='*' element={<NoMatch />} />
     </Routes>
   )
