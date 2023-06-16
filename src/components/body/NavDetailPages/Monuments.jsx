38d4d993fda285aaad1a8570b8eff0d3bcd6a8ea
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-06-16 10:33:36 +0300

    Minor Change

--- src/components/body/NavDetailPages/Monuments.jsx
+++ src/components/body/NavDetailPages/Monuments.jsx
@@ -4,15 +4,25 @@ import Navs from '../../navbars/navs'
 import '../css/Monuments.css'
 import {FcLike} from 'react-icons/fc';
 import {IoIosHeartDislike} from 'react-icons/io';
+const image_Data=[
+  {image:'m1.jpeg'},
+  {image:'m2.jpeg'},
+  {image:'m3.jpeg'},
+  {image:'m4.jpeg'},
+  {image:'m5.jpeg'},
+  {image:'m6.jpeg'},
+  {image:'m7.jpg'},
+  {image:'m8.jpeg'},
+]
 const monuments_data = [
-  { id:1,name: 'A', p:'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm1.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike /> } },
+  { id: 1,name: 'A', p:'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm1.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike /> } },
   { id: 2, name: 'A', p: 'In the centre of Ras Makonnen Sq stands a rather Italian-looking, equestrian statue of the ras cast in bronze by the well-known Amhara artist Afewerk Tekle. The ras is said to look towards Somalia and the lands conquered here.The ras is also known as the father of the Emperor Haile Selassie.', image: 'm2.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike /> } },
-  { id: 3, name: 'B', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm3.jpg', icons: { icon1: <FcLike/>, icon2: <IoIosHeartDislike />  } },
-  { id: 4, name: 'B', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm4.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike />  } },
-  { id: 5, name: 'B', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm5.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike />  } },
+  { id: 3, name: 'B', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm8.jpeg', icons: { icon1: <FcLike/>, icon2: <IoIosHeartDislike />  } },
+  { id: 4, name: 'B', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: '8.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike />  } },
+  { id: 5, name: 'B', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: '6.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike />  } },
   { id: 6, name: 'C', p: 'The Harar Jugol Wall is believed to have been built between the 13th and 16th centuries. The Harar Jugol Wall originally had five historic gates providing five entries into five different districts of the city. The wall was built as a defense in medieval times, an imposing 5m high and 3.5km long.', image: 'm6.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike />  } },
-  { id: 7, name: 'C', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm7.jpg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike />  } },
-  { id: 8, name: 'D', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: 'm8.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike /> } }
+  { id: 7, name: 'C', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: '1.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike />  } },
+  { id: 8, name: 'D', p: 'Ratione explicabo nam odit doloribus rem molestiae, maiores corrupti aperiam nesciunt ex adipisci, illum autem voluptatem ipsa. Numquam unde odit repudiandae nisi.', image: '5.jpeg', icons: { icon1: <FcLike />, icon2: <IoIosHeartDislike /> } }
 ];
 const Monuments = () => {
   useEffect(() => {
@@ -28,24 +38,15 @@ const Monuments = () => {
         </div>
         <h1 className='text-center font-mono text-[#3bb] text-3xl'>Free Gallary</h1>
         <div className="monument">
-        {
-          monuments_data.map((monument)=>(
-            <div className="layout">
-                <div className="image" style={{backgroundImage:`url(${monument.image})`}}>
-                  <div className="icon">
-                    <p className='hov-effect'>
-                      <span className='text-white text-xl'>Over View</span>
-                      <p className='text-white'>{monument.p}</p>
-                    </p>
-                  </div>
-                </div>
-                <div className="only-icons mt-2">
-                  <button className='text-xl mr-3 text-gray-400'>{monument.icons.icon1}</button>
-                  <button className='text-xl text-gray-400'>{monument.icons.icon2}</button>
-                </div>
+          {
+            monuments_data.map((src)=>(
+              <div className="">
+                <img src={src.image} alt="" />
+                <button className='mr-3'>{src.icons.icon1}</button>
+                <button>{src.icons.icon2}</button>
               </div>
-          ))
-        }
+            ))
+          }
         </div>
       </div>
       <Footer/>
