bfb24806091195ca73507a0b43b5c312d98a28ba
KaytrosGechoArka <kaytrosgech@gmail.com>
2023-06-19 15:02:09 +0300

    Minor Commit

--- src/components/body/NavDetailPages/NoMatch.jsx
+++ src/components/body/NavDetailPages/NoMatch.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { TbError404 } from 'react-icons/tb'
 const NoMatch = () => {
     const navigate = useNavigate();
   return (
     <>
-      <h1 className='ml-32 mt-11'>Page not found !</h1>
-      <button className='ml-32 mt-11 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={()=>navigate('/')}>Go to Home</button>
+      <div className="shadow-lg p-5 shadow-zinc-900 text-center absolute top-0 bottom-0 left-0 right-0 h-96 w-[50%] m-auto ">
+        <div className='my-[10%]'> 
+                
+          <h1 className='text-9xl m-auto pl-[41%]'><TbError404 className='text-blue-950'/></h1>
+          <p className='text-2xl'>Page Not Found!</p>
+          <button className=' bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => navigate('/')}>Go to Home</button>
+        </div>
+      </div>
     </>
   )
 }
