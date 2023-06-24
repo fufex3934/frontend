01603dd8f4e60c768ae971b5bbc63ee7560a796a
Fufa Wakgari <fufawakgari174@gmail.com>
2023-06-24 12:47:46 +0300

    events page

--- src/components/body/ServiceDepartment.jsx
+++ src/components/body/ServiceDepartment.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GoLaw } from 'react-icons/go'
 import { TbBusinessplan } from 'react-icons/tb'
 import { SiCultura } from 'react-icons/si'
@@ -7,36 +7,38 @@ import { MdPark, MdLocalPolice } from 'react-icons/md'
 import Navs from '../navbars/navs'
 import Footer from './Footer'
 import './css/ServiceDepartment.css'
-const departments=[
-  {
-    image: 'law.jpeg', icon: <GoLaw className='icon'/>, header:'Constution and Law',description:'Aim for Nation Building and defining national flag anthem and other symbols'
-  },
-  {
-    image: 'art.jpeg', icon: <SiCultura className='icon text-center' />, header: 'Art and Culture', description: 'If it is worth doing, we have got it. Find the center of tours and recreations'
-  },
-  {
-    image: '1.jpeg', icon: <TbBusinessplan className='icon' />, header: 'Finance and Economy', description:'Where we focus on how companies and investors evaluate risk and return'
-  },
-  {
-    image: 'bs.jpeg', icon: <IoBusiness className='icon' />, header: 'Business & Industry', description:'Whole economic companies, people, and organizations are in production field'
-  },
-  {
-    image: 'police.jpeg', icon: <MdLocalPolice className='icon' />, header: 'Policing and Crime', description: 'Maintain and protect citizens from crimes and dangerous incidents that are unlawful'
-  },
-  {
-    image: 'visit.jpeg', icon: <MdPark className='icon' />, header: 'Park and Recreation', description:'Our resources and services for the leisure, entertainment, and recreational pursuits'
-  }
-]
+import { useTranslation } from 'react-i18next'
 const ServiceDepartment = () => {
+  const {t} = useTranslation();
+  const departments=[
+    {
+      image: 'law.jpeg', icon: <GoLaw className='icon'/>, header:`${t('serviceDepartments.lawHeader')}`,description:`${t('serviceDepartments.lawdesc')}`,
+    },
+    {
+      image: 'art.jpeg', icon: <SiCultura className='icon text-center' />, header: `${t('serviceDepartments.artHeader')}`, description: `${t('serviceDepartments.artdesc')}`
+    },
+    {
+      image: '1.jpeg', icon: <TbBusinessplan className='icon' />, header: `${t('serviceDepartments.financeHeader')}`, description:`${t('serviceDepartments.financedesc')}`
+    },
+    {
+      image: 'bs.jpeg', icon: <IoBusiness className='icon' />, header: `${t('serviceDepartments.businessHeader')}`, description:`${t('serviceDepartments.businessdesc')}`
+    },
+    {
+      image: 'police.jpeg', icon: <MdLocalPolice className='icon' />, header: `${t('serviceDepartments.policeHeader')}`, description: `${t('serviceDepartments.policedesc')}`
+    },
+    {
+      image: 'visit.jpeg', icon: <MdPark className='icon' />, header: `${t('serviceDepartments.parkHeader')}`, description:`${t('serviceDepartments.parkdesc')}`
+    }
+  ]
   return (
     <>
       <Navs />
       <div className='communication'>
-        <h1>Government Communication Departments</h1>
+        <h1>{t('serviceDepartments.header')}</h1>
       </div>
       <div className="desc">
-        <h1>Our Goverment Departments</h1>
-        <p>We are offering the following information's about us that what we actually.</p>
+        <h1>{t('serviceDepartments.header2')}</h1>
+        <p>{t('serviceDepartments.p')}</p>
       </div>
       <div className='layout'>
       {
