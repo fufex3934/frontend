23a942f475495448798795f39670796101ed5684
Fufa Wakgari <fufawakgari174@gmail.com>
2023-07-03 08:54:19 +0300

    some commits

--- src/sanityAuth/CreateAccount.jsx
+++ src/sanityAuth/CreateAccount.jsx
@@ -34,8 +34,8 @@ const CreateAccount = () => {
     }
   };
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow max-w-md">
+    <div className="min-h-screen  bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow w-[500px]">
         <h1 className="text-3xl font-semibold mb-6 text-slate-800">Create Account</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
